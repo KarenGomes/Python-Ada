aa6b079a596f82a05c1cbccaae2827a0d3db4756
Karen Gomes <karenecris12015@gmail.com>
2023-11-11 19:35:34 -0300

    aula 8

---
+++ listas.py
@@ -0,0 +1,34 @@
+nota1= 7.9
+nota2 = 9.7
+nota3 = 8.2
+
+notas = [nota1, nota2, nota2]
+
+#criando listas 
+
+lista = []
+lista2 = list()
+lista3 = [25, 'nome', False]
+lista_de_listas = [10, [1,2,3]]
+
+#índice
+
+print(lista3[1])
+
+#slice
+
+lista = [10, 50, 30, 40, 25]
+print(lista[0:3]) #de 0 até 3
+print(lista[2:]) #2 até o final 
+
+
+for elemento in lista:
+    print(elemento)
+
+#comprimento 
+
+print('comprimento da lista ', len(lista))
+
+for i in range(len(lista)):
+    print(lista[i])
+
