020fce5a198754518188f20d535a12060d4329ec
Karen Gomes <karenecris12015@gmail.com>
2023-11-11 19:16:20 -0300

    aula 6

---
+++ while.py
@@ -0,0 +1,20 @@
+numero_sorteado = 15
+numero_escolhido = int(input('informe um numero entre 1 e 20: '))
+
+while numero_escolhido != numero_sorteado:
+    print('você errou o numero, tente novamente')
+
+    numero_escolhido = int(input('informe um numero entre 1 e 20: '))
+
+
+print('Parabéns você acertou')
+
+
+# Exemplo com contador
+
+contador = 0
+
+while contador < 5: 
+    print(contador)
+    contador = contador + 1
+
