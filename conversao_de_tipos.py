10db2294a40030ad5d746e35eb2d4934e1771d4d
Karen Gomes <karenecris12015@gmail.com>
2023-11-11 19:00:49 -0300

    aula 4

---
+++ conversao_de_tipos.py
@@ -0,0 +1,24 @@
+# CONVERSAO DE TIPOS
+
+idade = '26'
+numero1 = '10'
+numero2 = '20'
+
+print(idade, type(idade))
+
+idade_inteira = int(idade)
+
+print(idade_inteira, type(idade_inteira))
+
+"""
+int()
+str()
+float()
+bool()
+
+"""
+
+altura = float(input('informe a sua altura: ' ))    
+
+print(altura, type(altura))
+
