ad156474f21d1a0ca9202c934308a228201a565e
Karen Gomes <karenecris12015@gmail.com>
2023-11-11 18:55:27 -0300

    aula 3

---
+++ operadores.py
@@ -0,0 +1,22 @@
+# > OPERADORES ARITMÉTICOS 
+
+numero1 = 10
+numero2 = 20
+
+print(numero1 + numero2)
+print(numero1 - numero2)
+print(numero1 * numero2)
+print(numero1 / numero2)
+print(numero1 // numero2)
+print(20 % 3)
+print(2 ** 3 )
+
+# > OPERADORES BOOLEANOS
+
+idade1 = 10
+idade2 = 15
+
+print(idade1 > idade2)
+print(idade1<= idade2)
+print('Python'== 'python')
+print('banana'!= 'abacaxi')
