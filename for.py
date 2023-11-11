9454ab2a5e37a31fa486570dc7a17519b9343030
Karen Gomes <karenecris12015@gmail.com>
2023-11-11 19:27:01 -0300

    aula 7

---
+++ for.py
@@ -0,0 +1,21 @@
+"""
+for variavel in range(10):
+    print(variavel)
+
+
+for variavel in range (5, 11):
+    print(variavel)
+
+for variavel in range (1, 12, 3):
+    print(variavel)
+
+"""
+
+soma = 0
+
+for i in range(1,3):
+    nota = float(input(f'informe sua nota {i}:'))
+    soma = soma + nota
+
+print(soma/3)
+
