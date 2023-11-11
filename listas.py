97c030630e8dab1001eaa4b5e97322ce7574b342
Karen Gomes <karenecris12015@gmail.com>
2023-11-11 19:47:23 -0300

    aula 9

--- listas.py
+++ listas.py
@@ -32,3 +32,84 @@ print('comprimento da lista ', len(lista))
 for i in range(len(lista)):
     print(lista[i])
 
+# > METODOS DE LISTA
+
+
+lista = [1, 3, 12, 8, 2]
+
+#append 
+
+print('antes do append: ', lista)
+
+lista.append(3)
+
+print('depois do append', lista)
+
+#insert 
+
+lista.insert(2, 10)
+
+print('depois do insert', lista)
+
+#extend 
+
+lista2 = [1,2,3]
+
+lista.extend(lista2)
+
+print('depois do extend', lista)
+
+#pop
+
+lista.pop()
+
+print('Lista após o pop', lista)
+
+lista.pop(0)
+
+print('Lista após o pop indice 0', lista)
+
+#remove 
+
+lista.remove(3) #remove o elemento 
+
+print('Depois de remove', lista)
+
+#count 
+
+print('quantidade de 2 na lista: ', lista.count(2))
+
+#index
+
+print('indice do elemento 12: ', lista.index(12))
+
+#sort
+
+lista.sort()
+
+print('lista ordenada: ', lista)
+
+lista.sort(reverse=True)
+
+print('Lista reversa', lista)
+
+# FUNÇÕES PARA LISTAS 
+
+#len
+
+print(len(lista))
+
+#sum 
+
+print(sum(lista)) #soma todos os elementos da lista
+
+#max
+
+print('maior elemento da lista', max(lista))
+
+#min
+
+print('menor elemento da lista', min(lista))
+
+
+
