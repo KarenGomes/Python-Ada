63e9b3e087b9705bd4b2189ad1a9056b23a0ceae
Karen Gomes <karenecris12015@gmail.com>
2023-11-11 19:09:40 -0300

    aula 5

---
+++ condicionais.py
@@ -0,0 +1,22 @@
+# ESTRUTURAS CONDICIONAIS
+
+idade = 20
+
+if idade >= 18:
+    print('você é maior de idade')
+else: 
+    print('você é menor de idade')
+
+#Exercicio 1
+
+media = 10
+presenca = 100
+
+if media >= 7 and presenca >70:
+    print('Você foi aprovado!')
+elif media >= 5:
+    print('recuperação!')
+else:
+    print('Você foi reprovado!')
+
+
